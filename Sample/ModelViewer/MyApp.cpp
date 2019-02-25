065b1caf6ffcd0cc520088cda3baf5b62eefa0b1
mindbrain <xwz784574@hotmail.com>
2019-02-25 16:32:27 +0800

    修改ModelViewer例子程序

--- Sample/ModelViewer/MyApp.cpp
+++ Sample/ModelViewer/MyApp.cpp
@@ -9,10 +9,27 @@
 
 MyApp::MyApp()
 {
+	m_scene = nullptr;
+	m_camera = nullptr;
+	m_model = nullptr;
+
 	m_bLeftDown = false;
 	m_bRightDown = false;
 
+	m_model = nullptr;
+
 	m_elapsed = 0;
+
+	m_loopPlay = false;
+	m_playing = false;
+
+	m_currAnimIndex = 0;
+
+	m_playBtn = nullptr;
+	m_pauseBtn = nullptr;
+	m_currFrameLabel = nullptr;
+	m_totalFrameLabel = nullptr;
+	m_frameSlider = nullptr;
 }
 
 MyApp::~MyApp()
@@ -42,13 +59,6 @@ bool MyApp::Init()
 	m_scene = sSceneMgr->CreateScene();
 	sSceneMgr->SetCurrScene(m_scene);
 
-	SceneImporter* sceneImporter = sSceneMgr->CreateSceneImporter("Model/Ifrit/Ifrit.sceneml");
-	sceneImporter->ImportScene(m_scene);
-	SAFE_DELETE(sceneImporter);
-
-	SceneNode* sceneNode = m_scene->GetSceneNode(0);
-	m_model = sceneNode->GetModel(0);
-
 	m_camera = m_scene->GetSceneCamera();
 	m_camera->BuildViewMatrix(Vector3Df(0, 2.0f, -5.0f), Vector3Df(0, 1.0f, 0));
 	m_camera->BuildProjMatrix((float32)PI / 4, wndSize.width, wndSize.height, 1.0f, 1000.0f);
@@ -57,9 +67,24 @@ bool MyApp::Init()
 	//UI
 	//////////////////////////////////////////////////////////////////////////
 	sDesktopMgr->SetDesktopSize(wndSize.width, wndSize.height);
-	sDesktopMgr->RegisterControlViewer(this);
 	sDesktopMgr->LoadUILayoutFile("Interface/Console/Console.xml");
 
+	m_playBtn = (Button*)sDesktopMgr->GetControl("PlayAnim");
+	m_pauseBtn = (Button*)sDesktopMgr->GetControl("PauseAnim");
+	m_currFrameLabel = (FontString*)sDesktopMgr->GetControl("FrameBeginLabel");
+	m_totalFrameLabel = (FontString*)sDesktopMgr->GetControl("FrameEndLabel");
+	m_frameSlider = (Slider*)sDesktopMgr->GetControl("AnimFrame");
+
+	m_playBtn->SetHidden(false);
+	m_pauseBtn->SetHidden(true);
+
+	sDesktopMgr->RegisterControlViewer("PlayAnim", this, (EVENT_CALLBACK)&MyApp::onPlayAnimationClick);
+	sDesktopMgr->RegisterControlViewer("PauseAnim", this, (EVENT_CALLBACK)&MyApp::onPauseAnimationClick);
+	sDesktopMgr->RegisterControlViewer("PrevAnim", this, (EVENT_CALLBACK)&MyApp::onPrevAnimationClick);
+	sDesktopMgr->RegisterControlViewer("NextAnim", this, (EVENT_CALLBACK)&MyApp::onNextAnimationClick);
+	sDesktopMgr->RegisterControlViewer("LoopPlay", this, (EVENT_CALLBACK)&MyApp::onLoopPlayClick);
+	sDesktopMgr->RegisterControlViewer("OpenFile", this, (EVENT_CALLBACK)&MyApp::onOpenFileClick);
+	sDesktopMgr->RegisterControlViewer("AnimFrame", this, (EVENT_CALLBACK)&MyApp::onFrameSliderValueChange);
 	//////////////////////////////////////////////////////////////////////////
 	return true;
 }
@@ -115,100 +140,202 @@ bool MyApp::onKeyboardEvent(const KeyEvent& event)
 {
 	switch (event.key)
 	{
-		case KEY_A:
-		{
-			m_camera->Move(Vector3Df(-1.0f * m_elapsed * MOVE_SPEED, 0, 0));
-		}
-		break;
-		case KEY_D:
-		{
-			m_camera->Move(Vector3Df(1.0f * m_elapsed * MOVE_SPEED, 0, 0));
-		}
-		break;
-		case KEY_W:
-		{
-			m_camera->Move(Vector3Df(0, 0, 1.0f * m_elapsed * MOVE_SPEED));
-		}
-		break;
-		case KEY_S:
-		{
-			m_camera->Move(Vector3Df(0, 0, -1.0f * m_elapsed * MOVE_SPEED));
-		}
-		break;
-		case KEY_Q:
-		{
-			m_camera->Move(Vector3Df(0, 1.0f * m_elapsed * MOVE_SPEED, 0));
-		}
-		break;
-		case KEY_E:
-		{
-			m_camera->Move(Vector3Df(0, -1.0f * m_elapsed * MOVE_SPEED, 0));
-		}
-		break;
+	case KEY_A:
+	{
+		m_camera->Move(Vector3Df(-1.0f * m_elapsed * MOVE_SPEED, 0, 0));
+	}
+	break;
+	case KEY_D:
+	{
+		m_camera->Move(Vector3Df(1.0f * m_elapsed * MOVE_SPEED, 0, 0));
+	}
+	break;
+	case KEY_W:
+	{
+		m_camera->Move(Vector3Df(0, 0, 1.0f * m_elapsed * MOVE_SPEED));
+	}
+	break;
+	case KEY_S:
+	{
+		m_camera->Move(Vector3Df(0, 0, -1.0f * m_elapsed * MOVE_SPEED));
+	}
+	break;
+	case KEY_Q:
+	{
+		m_camera->Move(Vector3Df(0, 1.0f * m_elapsed * MOVE_SPEED, 0));
+	}
+	break;
+	case KEY_E:
+	{
+		m_camera->Move(Vector3Df(0, -1.0f * m_elapsed * MOVE_SPEED, 0));
+	}
+	break;
 	}
 
 	return true;
 }
 
-void MyApp::onControlCreate(const char8* name, int32 v1, float32 v2)
+bool MyApp::onPlayAnimationClick(UIObject* obj, const EventArgs& eventArgs)
 {
+	if (m_model == nullptr) return false;
 
+	m_model->PlayAnimation();
+
+	m_playBtn->SetHidden(true);
+	m_pauseBtn->SetHidden(false);
+
+	m_playing = true;
+
+	return true;
 }
 
-void MyApp::onControlValueChange(const char8* name, int32 v1, float32 v2)
+bool MyApp::onPauseAnimationClick(UIObject* obj, const EventArgs& eventArgs)
 {
-	static const char8* animNameList[] =
-	{
-		"atk1", "atk2", "atk3", "atk4", "bak1", "bak2", "bak3", "bko1", "blk",
-		"coma", "dead", "duck", "ent", "fko1", "jmp1", "jmp2", "jmp3", "lei1",
-		"lei2", "out", "rdy", "rsh", "rsha", "run", "runa", "sf01", "sf02",
-		"sk01", "sk02", "sk03", "sk04", "std", "stop", "stpa", "wlk", "wlka",
-	};
+	if (m_model == nullptr) return false;
 
-	static int32 animIndex = 0;
+	m_model->PauseAnimation();
 
-	static bool loop = false;
+	m_playBtn->SetHidden(false);
+	m_pauseBtn->SetHidden(true);
 
-	if (strcmp(name, "NextAnim") == 0)
-	{
-		animIndex++;
-		animIndex = animIndex % (sizeof(animNameList) / sizeof(const char8*));
+	m_playing = false;
 
-		m_model->SetCurrentAnimation(animNameList[animIndex]);
-	}
-	else if (strcmp(name, "PrevAnim") == 0)
-	{
-		animIndex--;
-		if (animIndex < 0) animIndex = (sizeof(animNameList) / sizeof(const char8*)) - 1;
-		animIndex = animIndex % (sizeof(animNameList) / sizeof(const char8*));
+	return true;
+}
 
-		m_model->SetCurrentAnimation(animNameList[animIndex]);
-	}
-	else if (strcmp(name, "PlayAnim") == 0)
+bool MyApp::onPrevAnimationClick(UIObject* obj, const EventArgs& eventArgs)
+{
+	if (m_model == nullptr) return false;
+
+	m_currAnimIndex--;
+	m_currAnimIndex = m_currAnimIndex < 0 ? (uint32)(m_animNameList.size() - 1) : m_currAnimIndex;
+
+	m_model->SetCurrentAnimation(m_animNameList[m_currAnimIndex].c_str());
+
+	SetSliderFromAnimation();
+
+	return true;
+}
+
+bool MyApp::onNextAnimationClick(UIObject* obj, const EventArgs& eventArgs)
+{
+	if (m_model == nullptr) return false;
+
+	m_currAnimIndex++;
+	m_currAnimIndex = m_currAnimIndex >= m_animNameList.size() ? 0 : m_currAnimIndex;
+
+	m_model->SetCurrentAnimation(m_animNameList[m_currAnimIndex].c_str());
+
+	SetSliderFromAnimation();
+
+	return true;
+}
+
+bool MyApp::onLoopPlayClick(UIObject* obj, const EventArgs& eventArgs)
+{
+	m_loopPlay = eventArgs.args[0].ba;
+
+	return true;
+}
+
+bool MyApp::onOpenFileClick(UIObject* obj, const EventArgs& eventArgs)
+{
+	OPENFILENAME ofn;
+	TCHAR szFileName[MAX_PATH] = _T("");
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = sPlatform->GetWindowsHandle();;    //父窗口句柄
+	ofn.lpstrFilter = _T("All Files (*.*)\0*.*\0");   //打开的文件类型，这里以xml和所有文件为例
+	ofn.lpstrFile = szFileName;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.Flags = OFN_FILEMUSTEXIST;
+	ofn.lpstrDefExt = _T("*");        //默认的打开的文件类型
+	ofn.lpstrInitialDir = _T(".\\");     //默认的打开的文件路径，这里以当前目录为例
+
+	if (!GetOpenFileName(&ofn)) return false;
+
+	char8 buf[MAX_PATH];
+	Utils::unicode_to_utf8((char16*)szFileName, -1, buf, MAX_PATH);
+
+	SceneImporter* sceneImporter = sSceneMgr->CreateSceneImporter(buf);// "Model/Ifrit/Ifrit.sceneml");
+	sceneImporter->ImportScene(m_scene);
+	SAFE_DELETE(sceneImporter);
+
+	SceneNode* sceneNode = m_scene->GetSceneNode(0);
+	m_model = sceneNode->GetModel(0);
+
+	for (uint32 i = 0; i < m_model->GetAnimationNum(); i++)
 	{
-		m_model->PlayAnimation(loop);
+		const char8* animName = m_model->GetAnimationName(i);
+		m_animNameList.push_back(animName);
 	}
-	else if (strcmp(name, "PauseAnim") == 0)
+
+	SetSliderFromAnimation();
+
+	return true;
+}
+
+bool MyApp::onFrameSliderValueChange(UIObject* obj, const EventArgs& eventArgs)
+{
+	if (!m_playing)
 	{
-		m_model->PauseAnimation();
+		if (m_model == nullptr) return false;
+
+		m_model->SetCurrentAnimationFrame((uint32)eventArgs.args[0].fa);
 	}
-	else if (strcmp(name, "StopAnim") == 0)
-	{
-		m_model->StopAnimation();
-	}
-	else if (strcmp(name, "LoopPlay") == 0)
-	{
-		loop = (v1 == 0 ? false : true);
-	}
-	else if (strcmp(name, "AnimSpeed") == 0)
-	{
-		m_model->SetPlayAnimationSpeed(v2);
-	}
+
+	return true;
+}
+
+void MyApp::SetSliderFromAnimation()
+{
+	uint32 frameNum = m_model->GetCurrentAnimationFrameNum();
+
+	m_frameSlider->SetValueRange(0, (float32)frameNum - 1, 1);
+	m_frameSlider->SetValue(0);
+
+	m_currFrameLabel->SetText("0");
+
+	char8 buf[32];
+	sprintf(buf, "%d", frameNum - 1);
+	m_totalFrameLabel->SetText(buf);
 }
 
 void MyApp::Update(float32 fElapsed)
 {
 	m_elapsed = fElapsed;
+
+	if (m_model == nullptr) return;
+
+	uint32 frame = m_model->GetCurrentAnimationFrame();
+
+	if (m_playing)
+	{
+		m_frameSlider->SetValue((float32)frame);
+
+		uint32 animFrameNum = m_model->GetCurrentAnimationFrameNum();
+		uint32 currFrame = m_model->GetCurrentAnimationFrame();
+
+		if (currFrame >= animFrameNum - 1)
+		{
+			if (m_loopPlay)
+			{
+				m_model->SetCurrentAnimationFrame(0);
+				m_model->PlayAnimation();
+			}
+			else
+			{
+				m_playBtn->SetHidden(false);
+				m_pauseBtn->SetHidden(true);
+
+				m_playing = false;
+			}
+		}
+	}
+
+	char8 buf[32];
+	sprintf(buf, "%d", frame);
+	m_currFrameLabel->SetText(buf);
 }
 
 void MyApp::onRender(float32 elapsed)
--- Sample/ModelViewer/MyApp.h
+++ Sample/ModelViewer/MyApp.h
@@ -1,31 +1,13 @@
 #pragma once
 
-class MyApp : public IApplication, public IRenderCallback, public IControlViewer
+//IControlViewer放在第一位，防止出现IControlViewer中类成员函数指针注册时出现的编译警告
+class MyApp : public IControlViewer, public IRenderCallback, public IApplication
 {
 public:
 	MyApp();
 	virtual ~MyApp();
 
 private:
-	struct Vertex
-	{
-		Vertex(const Vector3Df& pos, const Vector2Df& tex)
-		{
-			this->pos = pos;
-			this->tex = tex;
-		}
-		Vector3Df	pos;
-		Vector2Df	tex;
-	};
-
-private:
-	Renderer*				m_renderer;
-	RenderFrameBuffer*		m_defFrameBuf;
-
-	RenderLayout*			m_copyLayout;
-	RenderProgram*			m_copyShader;
-	RenderTechnique*		m_copyTech;
-
 	OctreeScene*			m_scene;
 	SceneCamera*			m_camera;
 	Model*					m_model;
@@ -33,6 +15,29 @@ private:
 	bool			m_bRightDown;
 
 	float32			m_elapsed;
+
+	bool		m_loopPlay;
+	bool		m_playing;
+
+	vector<string>		m_animNameList;
+	int32				m_currAnimIndex;
+
+	Button*				m_playBtn;
+	Button*				m_pauseBtn;
+	FontString*			m_currFrameLabel;
+	FontString*			m_totalFrameLabel;
+	Slider*				m_frameSlider;
+
+private:
+	virtual bool onPlayAnimationClick(UIObject* obj, const EventArgs& eventArgs);
+	virtual bool onPauseAnimationClick(UIObject* obj, const EventArgs& eventArgs);
+	virtual bool onPrevAnimationClick(UIObject* obj, const EventArgs& eventArgs);
+	virtual bool onNextAnimationClick(UIObject* obj, const EventArgs& eventArgs);
+	virtual bool onLoopPlayClick(UIObject* obj, const EventArgs& eventArgs);
+	virtual bool onOpenFileClick(UIObject* obj, const EventArgs& eventArgs);
+	virtual bool onFrameSliderValueChange(UIObject* obj, const EventArgs& eventArgs);
+
+	virtual void SetSliderFromAnimation();
 
 public:
 	virtual bool Init();
@@ -44,9 +49,6 @@ public:
 
 	virtual bool onMouseEvent(const MouseEvent& event);
 	virtual bool onKeyboardEvent(const KeyEvent& event);
-
-	virtual void onControlCreate(const char8* name, int32 v1, float32 v2);
-	virtual void onControlValueChange(const char8* name, int32 v1, float32 v2);
 
 	virtual void Update(float32 fElapsed);
 	virtual void onRender(float32 elapsed);
