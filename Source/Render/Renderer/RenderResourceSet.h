cd2a2055319bd9f42c9f1e383077cfee64af6e19
v_xiaowenzhang <v_xiaowenzhang@baidu.com>
2020-08-11 20:26:22 +0800

    改进渲染器接口

--- Source/Render/Renderer/RenderResourceSet.h
+++ Source/Render/Renderer/RenderResourceSet.h
@@ -10,13 +10,13 @@ namespace Pharos
             virtual ~RenderResourceSet(){}
             
         public:			
-            virtual void SetVertexUniformBuffer(uint32 slot, RenderBuffer* buffer) = 0;
-            virtual void SetFragmentUniformBuffer(uint32 slot, RenderBuffer* buffer) = 0;
+            virtual bool SetVertexUniformBuffer(uint32 slot, RenderBuffer* buffer) = 0;
+            virtual bool SetFragmentUniformBuffer(uint32 slot, RenderBuffer* buffer) = 0;
 
-            virtual void SetVertexTexture(uint32 slot, RenderTexture* tex) = 0;
-            virtual void SetFragmentTexture(uint32 slot, RenderTexture* tex) = 0;
+            virtual bool SetVertexTexture(uint32 slot, RenderTexture* tex) = 0;
+            virtual bool SetFragmentTexture(uint32 slot, RenderTexture* tex) = 0;
 
-            virtual bool UpdateSet() = 0;
+            virtual void UpdateSet() = 0;
         };
     }
 }
